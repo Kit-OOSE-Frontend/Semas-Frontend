4e37fc927c5670d2ef3cf130e0ba6c8edf413407
haizena <im7856@naver.com>
2024-06-22 03:32:41 +0900

    modified:   src/App.js 	modified:   src/page/AddBudgetingDetail.js 	modified:   src/page/InquiryBudgetingDetail.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import AddBudgetingDetail from './page/AddBudgetingDetail';
 import AddCareerDetail from './page/AddCareerDetail';
 import { Route, Routes } from 'react-router-dom';
+import InquiryBudgetingDetail from './page/InquiryBudgetingDetail';
+import InquiryCareerDetail from './page/InquiryCareerDetail';
 
 function App() {
     return (
         <Routes>
             <Route path="/budget" element={<AddBudgetingDetail />} />
             <Route path="/career" element={<AddCareerDetail />} />
+            <Route path="/inquirybudget" element={<InquiryBudgetingDetail />} />
+            <Route path="/inquirycareer" element={<InquiryCareerDetail />} />
         </Routes>
     );
 }
