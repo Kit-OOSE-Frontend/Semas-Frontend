690ddc54fb8044d07ba6dcb457d9fbfb489fd6f7
hodu42 <hodu42@DESKTOP-55B75E0>
2024-06-22 20:47:51 +0900

    [Fix] merge 후 오류 발생

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import RetirementPensionCalculation from "./page/RetirementPensionCalculation";
 import InquirySeveranceAmount from "./page/InquirySeveranceAmount";
 import AddBudgetingDetail from './page/AddBudgetingDetail';
 import AddCareerDetail from './page/AddCareerDetail';
-import { Route, Routes } from 'react-router-dom';
 import InquiryBudgetingDetail from './page/InquiryBudgetingDetail';
 import InquiryCareerDetail from './page/InquiryCareerDetail';
+import ContractLedger from "./contract/ContractLedger";
+import InputContract from "./contract/InputContract";
+import InquiryAsset from "./asset/InquiryAsset";
+import UpdateAssetStatement from "./asset/UpdateAssetStatement";
 
 function App() {
   return (
@@ -16,11 +19,11 @@ function App() {
         <Route path="/add" element={<AddDetailsOfExecution/>} />
         <Route path="/inquiry" element={<InquiryOperatingExpenses/>} />
         <Route path="/retire" element={<RetirementPensionCalculation/>} />
-        <Route path="/" element={<InquirySeveranceAmount/>} />
-      <Route path='/contract-ledger' element={<ContractLedger/>}/>
-      <Route path='/input-contract' element={<InputContract/>}/>
-      <Route path='/inquiry-asset' element={<InquiryAsset/>}/>
-      <Route path='/update-asset' element={<UpdateAssetStatement/>}/>
+          <Route path="/" element={<InquirySeveranceAmount/>} />
+          <Route path='/contract-ledger' element={<ContractLedger/>}/>
+          <Route path='/input-contract' element={<InputContract/>}/>
+          <Route path='/inquiry-asset' element={<InquiryAsset/>}/>
+          <Route path='/update-asset' element={<UpdateAssetStatement/>}/>
           <Route path="/budget" element={<AddBudgetingDetail />} />
           <Route path="/career" element={<AddCareerDetail />} />
           <Route path="/inquirybudget" element={<InquiryBudgetingDetail />} />
